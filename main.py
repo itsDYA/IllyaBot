54dc681996809889c5ea66108111041b88cfa1e5
itsDYA <carljosenpyt@gmail.com>
2021-01-26 12:47:21 +0000

    a

--- main.py
+++ main.py
@@ -2,22 +2,46 @@ import discord
 import os
 import requests
 import json
+import random
+from keep_alive import keep_alive
+
 
 client = discord.Client()
+basura = ["rezero"]
+verdad = [
+    "es una mierda",
+    "no debería existir",
+    "VIVA K-ON",
+]
+
 
 def get_quote():
-  response = request.get (https://myanimelist.net)
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " - " + json_data[0]['a']
+    return (quote)
+
 
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-      return
-  if message.content.startswith('Hola'):
-    await message.channel.send('Sup nerd')
+    if message.author == client.user:
+        return
 
+    msg = message.content
 
+    if msg.startswith('Hola'):
+        await message.channel.send('Sup nerd')
+
+    if msg.startswith('Inspirame'):
+        quote = get_quote()
+        await message.channel.send(quote)
+    if any(word in msg for word in basura):
+        await message.channel.send(random.choice(verdad))
+
+keep_alive()
 client.run(os.getenv('TOKEN'))
