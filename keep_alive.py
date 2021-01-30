1a4389eef61e0260fa23b1a08ddfcc8bb98a1042
itsDYA <carljosenpyt@gmail.com>
2021-01-30 16:20:59 +0000

    A

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "<head><title>IllyaBot</title></head><h1>Estado: Online!</h1><a href='https://pastebin.com/e6ShLfj1'>Comandos</a>"
+    return "<head><title>IllyaBot</title></head><img src='https://littlecloudcuriosity.files.wordpress.com/2014/07/fate-kaleid-liner-prisma-illya-2wei-episode-2-21.jpg' style='height:50vh; max-width:50%; object-fit: contain;'><h1>Estado: Online</h1><a href='https://pastebin.com/e6ShLfj1'>Comandos</a><br><a href='https://discord.com/api/oauth2/authorize?client_id=803354223538208869&permissions=8&scope=bot'>Añadir a tu servidor</a>"
 
 def run():
   app.run(host='0.0.0.0',port=8080)
--- main.py
+++ main.py
@@ -4,68 +4,91 @@ import os
 from discord.ext import commands
 from keep_alive import keep_alive
 
-client = commands.Bot(command_prefix = '&')
-basura = ["rezero", "Rezero","re zero", "Re zero"]
+players = {}
+client = commands.Bot(command_prefix='&')
+basura = ["rezero", "Rezero", "re zero", "Re zero"]
 verdad = [
-    "es una mierda",
-    "no debería existir",
-    "VIVA K-ON",
+    "es una mierda", "no debería existir", "VIVA K-ON",
     "mejor mira algun cgdct"
 ]
 saludos = ["hey", "hola", "o/", "Hey", "Hola"]
 
-
-
 @client.event
 async def on_ready():
-  await client.change_presence(status=discord.Status.online,activity=discord.Game('Fate/kaleid liner Prisma☆Illya: Licht - Namae no Nai Shoujo'))
-  print('Bot is ready.')
+	await client.change_presence(
+	    status=discord.Status.online,
+	    activity=discord.Game(
+	        'Fate/kaleid liner Prisma☆Illya: Licht - Namae no Nai Shoujo'))
+	print('Bot is ready.')
 
-@client.command(aliases = ['8ball'])
-async def _8ball(ctx,*, question):
-  responses = [
-    'En mi opinión, sí',
-    'Es cierto',
-    'Es decididamente así',
-    'Probablemente',
-    'Buen pronóstico',
-    'Todo apunta a que sí',
-    'Sin duda',
-    'Sí',
-    'Sí - definitivamente',
-    'Debes confiar en ello',
-    'No cuentes con ello',
-    'Mi respuesta es no',
-    'Mis fuentes me dicen que no',
-    'No parece serlo',
-    'Muy dudoso',
-    'Pregunta de nuevo']
-  await ctx.send(f'Pregunta: {question}\nRespuesta: {random.choice(responses)}')
+
+@client.command(aliases=['8ball'])
+async def _8ball(ctx, *, question):
+	responses = [
+	    'En mi opinión, sí', 'Es cierto', 'Es decididamente así',
+	    'Probablemente', 'Buen pronóstico', 'Todo apunta a que sí', 'Sin duda',
+	    'Sí', 'Sí - definitivamente', 'Debes confiar en ello',
+	    'No cuentes con ello', 'Mi respuesta es no',
+	    'Mis fuentes me dicen que no', 'No parece serlo', 'Muy dudoso',
+	    'Pregunta de nuevo'
+	]
+	await ctx.send(
+	    f'Pregunta: {question}\nRespuesta: {random.choice(responses)}')
+
+
+client.counter = 0
+
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-    msg = message.content
-    if msg.startswith('&'):
-      return await client.process_commands(message)
-    
-    if any(word in msg for word in saludos):
-          await message.channel.send('Sup nerd')
+	if message.author == client.user:
+		return
 
-    if any(word in msg for word in basura):
-          await message.channel.send(random.choice(verdad))
+	msg = message.content
+	if msg.startswith('&'):
+		return await client.process_commands(message)
 
-    
-    
+	if any(word in msg for word in saludos):
+		await message.channel.send('Sup nerd')
+
+	if client.user.mentioned_in(message):
+		await message.channel.send('No me molestes, estoy preparandome para mi futuro')
+
+	if any(word in msg for word in basura):
+		await message.channel.send(random.choice(verdad))	
+
+
+	await client.process_commands(message)
+
 
 @client.command()
 async def ping(ctx):
-  await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+	await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
 
 @client.command()
 async def home(ctx):
-  await ctx.send('https://illyabot.itsdya.repl.co/')
+	await ctx.send('https://illyabot.itsdya.repl.co/')
+
+
+@client.command()
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount=5):
+	await ctx.channel.purge(limit=amount + 1)
+
+
+@client.command()
+@commands.is_owner()
+async def clean(ctx, amount=10):
+	await ctx.channel.purge(limit=amount + 1)
+
+
+@client.command()
+@commands.is_owner()
+async def say(ctx, *, palabra):
+	await ctx.message.delete()
+	await ctx.send(f'{palabra}')
+
 
 
 
